bdec54cd7f8fd9dff815a31721e5220642bea2fd
Alwin Doss <alwindoss84@gmail.com>
2017-05-30 20:56:51 +0530

    Modified the application description

--- cmd/root.go
+++ cmd/root.go
@@ -33,16 +33,13 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "tolstoy",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A Learning Management System Server",
+	Long: `Tolstoy is a Learning Management System Server
+	This server exposes REST endpoints for any kind of client to consume
+	`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -74,8 +71,8 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".tolstoy") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AddConfigPath("$HOME")    // adding home directory as first search path
+	viper.AutomaticEnv()            // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
